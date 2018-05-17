ff4663e418e95c1719a466076b3ebebd988984be
Chris Henry <cdhenry@gmail.com>
2018-05-17 10:50:05 +0000

    automatically backed up by learn

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -2,10 +2,10 @@ export default function shoppingListItemReducer(state = {
   items: []
 }, action) {
   switch(action.type) {
- 
+
     case 'INCREASE_COUNT':
       return state.items.concat(state.items.length + 1);
- 
+
     default:
       return state;
   }
